bb3f89beb362a584374b2b8b5df3d7d7537dc5ad
phkuzyanin <phkuzyanin@gmail.com>
2023-11-27 21:56:28 +0300

    fin

---
+++ main.js
@@ -0,0 +1,28 @@
+const todoForm = document.querySelector('.todo__form');
+const todoList = document.querySelector('.todo__list');
+todoForm.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+  let newDiv = document.createElement('div');
+  newDiv.classList.add('todo__item');
+  newDiv.innerHTML = `
+  	<form action="" class="todo__buttons">
+		<buttons class="completed"><img src="./img/done.svg" alt="completed"></buttons>
+		<buttons class="delete"><img src="./img/delete.svg" alt="delete"></buttons>
+	</form>
+    <h2 class="todo__item-header">${todoForm.querySelector('#todo__header').value || ''}</h2>
+    <p class="todo__item-content">${todoForm.querySelector('#todo__content').value || ''}</p>
+  `;
+  todoList.appendChild(newDiv);
+  	let completed = document.getElementsByClassName('completed');
+	let deleted = document.getElementsByClassName('delete');
+	for(let element of completed){
+		element.addEventListener('click', function(event){
+			element.closest('.todo__item').classList.add('blurred');
+		})
+	}
+	for(let element of deleted){
+		element.addEventListener('click', function(event){
+			element.closest('.todo__item').remove();
+		})
+}
+});
