f9b49fa96f04d7bc541c0bad23c959462feedcdd
Philipp Kuzyanin <115359396+phkuzyanin@users.noreply.github.com>
2023-12-27 21:14:31 +0300

    v1.0. no bootstrap

--- main.js
+++ main.js
@@ -5,19 +5,22 @@ todoForm.addEventListener('submit', function(event) {
   let newDiv = document.createElement('div');
   newDiv.classList.add('todo__item');
   newDiv.innerHTML = `
+  <div class="todo__item-mask">
   	<form action="" class="todo__buttons">
 		<buttons class="completed"><img src="./img/done.svg" alt="completed"></buttons>
 		<buttons class="delete"><img src="./img/delete.svg" alt="delete"></buttons>
 	</form>
     <h2 class="todo__item-header">${todoForm.querySelector('#todo__header').value || ''}</h2>
     <p class="todo__item-content">${todoForm.querySelector('#todo__content').value || ''}</p>
+  <div class="todo__item-mask">
   `;
   todoList.appendChild(newDiv);
   	let completed = document.getElementsByClassName('completed');
 	let deleted = document.getElementsByClassName('delete');
 	for(let element of completed){
 		element.addEventListener('click', function(event){
-			element.closest('.todo__item').classList.add('blurred');
+			element.closest('.todo__item-mask').classList.add('blurred');
+			element.closest('.todo__item').style.borderColor= 'green';
 		})
 	}
 	for(let element of deleted){
